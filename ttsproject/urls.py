44b0994608d1736e293f2b0e521d6b767ef2cd45
ryutarotus <ryutaro03100310@icloud.com>
2022-07-31 00:28:33 +0900

    add ttslearn

--- ttsproject/urls.py
+++ ttsproject/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.static import serve
+#from django.views.static import serve
 import sys
 sys.path.append('../')
 from ttsapp.views import my_customized_server_error
@@ -26,7 +26,6 @@ from ttsapp.views import my_customized_server_error
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ttsapp.urls')),
-    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
