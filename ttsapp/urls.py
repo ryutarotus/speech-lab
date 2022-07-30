c53ca12d150e4866b871e95f51d75c73b3ea3bf3
ryutarotus <ryutaro03100310@icloud.com>
2022-07-30 12:08:41 +0900

    deploy heroku

--- ttsapp/urls.py
+++ ttsapp/urls.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from .views import demo_func, home_func, author_func, multi_func, my_customized_server_error
 
-handler500 = my_customized_server_error
-
 urlpatterns = [
     #path('admin/', admin.site.urls),
     path('demo/', demo_func, name="demo"),
@@ -11,3 +9,5 @@ urlpatterns = [
     path('home/', home_func, name="home"),
     path('author/', author_func, name="author"),
 ]
+
+handler500 = my_customized_server_error
