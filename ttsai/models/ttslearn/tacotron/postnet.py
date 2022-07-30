75fed8ef6d10bdcc76b8d2d7ef977a090460e16c
ryutarotus <ryutaro03100310@icloud.com>
2022-07-30 22:06:24 +0900

    add ttslearn

---
+++ ttsai/models/ttslearn/tacotron/postnet.py
@@ -0,0 +1,53 @@
+from torch import nn
+
+
+class Postnet(nn.Module):
+    """Post-Net of Tacotron 2
+
+    Args:
+        in_dim (int): dimension of input
+        layers (int): number of layers
+        channels (int): number of channels
+        kernel_size (int): kernel size
+        dropout (float): dropout rate
+    """
+
+    def __init__(
+        self,
+        in_dim,
+        layers=5,
+        channels=512,
+        kernel_size=5,
+        dropout=0.5,
+    ):
+        super().__init__()
+        postnet = nn.ModuleList()
+        for layer in range(layers):
+            in_channels = in_dim if layer == 0 else channels
+            out_channels = in_dim if layer == layers - 1 else channels
+            postnet += [
+                nn.Conv1d(
+                    in_channels,
+                    out_channels,
+                    kernel_size,
+                    stride=1,
+                    padding=(kernel_size - 1) // 2,
+                    bias=False,
+                ),
+                nn.BatchNorm1d(out_channels),
+            ]
+            if layer != layers - 1:
+                postnet += [nn.Tanh()]
+            postnet += [nn.Dropout(dropout)]
+        self.postnet = nn.Sequential(*postnet)
+
+    def forward(self, xs):
+        """Forward step
+
+        Args:
+            xs (torch.Tensor): input sequence
+
+        Returns:
+            torch.Tensor: output sequence
+        """
+        return self.postnet(xs)
