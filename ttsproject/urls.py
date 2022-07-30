45b6557b41d9ccbcf06621984d0e1ae8d760453d
ryutarotus <ryutaro03100310@icloud.com>
2022-07-30 12:35:22 +0900

    deploy heroku

--- ttsproject/urls.py
+++ ttsproject/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from ttsproject.views import my_customized_server_error
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('ttsapp.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) \
  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+handler500 = my_customized_server_error
